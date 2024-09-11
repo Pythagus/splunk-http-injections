1a74b0b3b8d4fbaef787e89011c71f001942b0e3
Pythagus <34168890+Pythagus@users.noreply.github.com>
2024-09-11 19:11:13 +0200

    :art: Update patterns

--- rules/patterns.py
+++ rules/patterns.py
@@ -32,14 +32,21 @@ patterns_lfi = {
 
     # Linux system files.
     "LFI_2": r"(^|\/)\.(ssh|git|bash|env|config|htaccess|htpasswd|composer|ansible|babel|composer|config|eslint|putty|idea|docker|appledb|xauthority|xdefaults|xresources|passwd)", # Hidden files/folders like .git
-    "LFI_3": r"(\W|)((boot|php|system|desktop|win)\.ini|httpd\.conf|(\/|\\)(log|logs)(\/|\\)(access|error)(\.|\_)log)", # Sensitive files
-    "LFI_4": r"(\W|)(var|etc|usr|proc|logs)(\/|\\)+(log|www|mail|bin|lib|run|spool|local|self|httpd|apache|passwd|shadow|nginx|mysql|host|security|ssh|adm|cpanel|ports|sbin|([a-z0-9\-]+)(\.d|ftpd))", # Sensitive Linux folders
+    "LFI_3": r"\b((boot|php|system|desktop|win)\.ini|httpd\.conf|(log|logs)(\/|\\)(access|error)(\.|\_)log)", # Sensitive files
+    "LFI_4": r"(\b|\/|\\)(var|etc|usr|proc|logs)(\/|\\)+(log|www|mail|bin|lib|run|spool|local|self|httpd|apache|passwd|shadow|nginx|mysql|host|security|ssh|adm|cpanel|ports|sbin|([a-z0-9\-]+)(\.d|ftpd))", # Sensitive Linux folders
     "LFI_5": r"\.(pem|py|key|yml|sh|ppk|p12|conf|sqlite|sqlitedb|sql|db|sql\.\w+|tar|tar\.\w+|war|rar|7z|bz2|lz|swp)$", # Suspicious files that shouldn't be accessed via URL.
 
     # Other files.
-    "LFI_WINDOWS_FILESYSTEM": r"(\W|)(c|d):((\/|\\)|windows|users|program)", # Windows system files.
-    "LFI_GIT_FILES": r"\/(readme|changes|changelog|code_of_conduct|versioning|license|contributing)\.md", # Git files.
-    "LFI_LIBRAIRY_FILES": r"^(?:\/)?vendor(\/|\\)", # PHP librairy files.
+    "LFI_WINDOWS_FILESYSTEM": r"\b(c|d):((\/|\\)|windows|users|program)", # Windows system files.
+    "LFI_GIT_FILES": r"(^|\/|\\)(readme|changes|changelog|code_of_conduct|versioning|license|contributing)\.md", # Git files.
+    "LFI_LIBRAIRY_FILES": r"^(\/|\\)*vendor(\/|\\)", # PHP librairy files.
+
+    # Well-known folder scripts.
+    # Examples:
+    #  - /.well-known/mini.php
+    #  - /.well-known/admin.php
+    #  - /cgi-bin/admin.php
+    "LFI_KNOWN_FOLDERS_SCRIPTS": r"(\.well-known|cgi-bin)(\/|\\)(.*)\.(php)",
 }
 
 #
@@ -55,12 +62,12 @@ patterns_lfi = {
 # See: https://owasp.org/www-community/attacks/SQL_Injection
 #
 patterns_sqli = {
-    "SQL_1": r"(\W|)(or|and|having|where)(\s|:|\(|\"|\')*[0-9xya]+(\)|\"|\'|\s)*=+(\s|\(|\"|\')*[0-9axy]+", # AND/OR 1=1
-    "SQL_2": r"(\W|)(select|insert|update|delete|drop|alter|create|union|waitfor(\s+)delay|(order(\s+)by))\s+", # SQL keywords
-    "SQL_3": r"(\W|)(pg_sleep|benchmark|randomblob|sleep|concat|concat_ws|extractvalue|updatexml|tdesencrypt|md5|chr)(\s*)\(", # SQL functions
-    "SQL_4": r"(\W|)(or|and)((\s+)true|(.*)is(\s*)null)", # Arithmetic evaluation
-    "SQL_5": r"(\W|)(xp_cmdshell|xp_enumgroups|xp_grantlogin|xp_logevent|xp_logininfo|xp_msver|xp_revokelogin|xp_sprintf|xp_sqlmaint|xp_sscanf|xp_regread)", # Microsoft SQL server commands
-    "SQL_6": r"(\W|)(bin|ord|hex|char)(\s*)(\(|\[)", # SQL 'arithmetic' functions
+    "SQL_1": r"\b(or|and|having|where)(\s|:|\(|\"|\')*[0-9xya]+(\)|\"|\'|\s)*=+(\s|\(|\"|\')*[0-9axy]+", # AND/OR 1=1
+    "SQL_2": r"\b(select|insert|update|delete|drop|alter|create|union|waitfor(\s+)delay|(order(\s+)by))\s+", # SQL keywords
+    "SQL_3": r"\b(pg_sleep|benchmark|randomblob|sleep|concat|concat_ws|extractvalue|updatexml|tdesencrypt|md5|chr)(\s*)\(", # SQL functions
+    "SQL_4": r"\b(or|and)((\s+)true|(.*)is(\s*)null)", # Arithmetic evaluation
+    "SQL_5": r"\b(xp_cmdshell|xp_enumgroups|xp_grantlogin|xp_logevent|xp_logininfo|xp_msver|xp_revokelogin|xp_sprintf|xp_sqlmaint|xp_sscanf|xp_regread)", # Microsoft SQL server commands
+    "SQL_6": r"\b(bin|ord|hex|char)(\s*)(\(|\[)", # SQL 'arithmetic' functions
 }
 
 #
@@ -78,12 +85,12 @@ patterns_sqli = {
 # See: https://owasp.org/www-community/attacks/xss/
 patterns_xss = {
     "XSS_1": r"<(\s*)script(\s*)", # <script> tags
-    "XSS_2": r"(\W|)on([a-z]+)(\s|=| |})", # Javascript keywords
-    "XSS_3": r"(\W|)(throw|alert|prompt)(\s*)(\s|{|\(|\)|\`)", # Some Javascript functions like alert(3)
-    "XSS_4": r"(\W|)(window|top)(\s*)\[[^\]]*\]", # Some javascript functions like top['alert']
-    "XSS_5": r"(\W|)(java|live|vb|j|w)script(\s*):", # Scripting languages
-    "XSS_6": r"(\W|)(document\.(domain|window|write|cookie|location)|response\.write)", # Document functions
-    "XSS_FUNCTION_RENAME": r"(\W|)[a-z]=[a-z]+,[a-z]\([^\)]*\)(\W|$)", # Function renaming like a=alert,a(1)
+    "XSS_2": r"\bon([a-z]+)(\s|=| |})", # Javascript keywords
+    "XSS_3": r"\b(throw|alert|prompt)(\s*)(\s|{|\(|\)|\`)", # Some Javascript functions like alert(3)
+    "XSS_4": r"\b(window|top)(\s*)\[[^\]]*\]", # Some javascript functions like top['alert']
+    "XSS_5": r"\b(java|live|vb|j|w)script(\s*):", # Scripting languages
+    "XSS_6": r"\b(document\.(domain|window|write|cookie|location)|response\.write)", # Document functions
+    "XSS_FUNCTION_RENAME": r"\b[a-z]=[a-z]+,[a-z]\([^\)]*\)(\W|$)", # Function renaming like a=alert,a(1)
 }
 
 #
@@ -106,16 +113,16 @@ patterns_rce = {
     
     # PHP code.
     "RCE_PHP_1": r"<\?php", # <?php
-    "RCE_PHP_2": r"(\W|)(phpinfo|phpversion|system|passthru|exec|shell_exec|backticks|base64_decode|sleep)(\s*)\(", # i.e. phpinfo()
-    "RCE_PHP_3": r"(\W|)\$\_(server|get|post|files|cookie|session|request|env|http_get_vars|http_post_vars)", # Global variables: https://www.php.net/manual/en/language.variables.superglobals.php
+    "RCE_PHP_2": r"\b(phpinfo|phpversion|system|passthru|exec|shell_exec|backticks|base64_decode|sleep)(\s*)\(", # i.e. phpinfo()
+    "RCE_PHP_3": r"\b\$\_(server|get|post|files|cookie|session|request|env|http_get_vars|http_post_vars)", # Global variables: https://www.php.net/manual/en/language.variables.superglobals.php
     "RCE_PHP_4": r"(bzip2|expect|glob|phar|ogg|rar|ssh2|zip|zlib|file|php):\/\/", # PHP local file wrapper.
-    "RCE_PHP_COMMANDS_1": r"(\W|)allow_url_(fopen|include)\W",
+    "RCE_PHP_COMMANDS_1": r"\ballow_url_(fopen|include)\W",
 
     # Java code.
     "RCE_JAVA_1": r"\bjava\.(io|lang|net|util)\.",
 
     # Bash code.
-    "RCE_OS_1": r"(\W|)(echo|nslookup|printenv|which|wget|curl|whoami|ping|uname|systeminfo|sysinfo|ifconfig|sleep|perl|netstat|ipconfig|nc|net(\s+)(localgroup|user|view)|netsh|dir|ls|pwd)\b",
+    "RCE_OS_1": r"\b(echo|nslookup|printenv|which|wget|curl|whoami|ping|uname|systeminfo|sysinfo|ifconfig|sleep|perl|netstat|ipconfig|nc|net(\s+)(localgroup|user|view)|netsh|dir|ls|pwd)(\s|{|\()",
 
     # Windows.
     "RCE_WINDOWS_1": r"(%systemroot%|hklm\\system\\)",
@@ -149,7 +156,7 @@ pattern_user_agent = r"(API-Spyder|Cyberint|HTTP Banner Detection|Nacos|bitdisco
 pattern_xff = r"^(((([0-9\.]{1,3}\.){3}[0-9]{1,3}|(?:\[)?[0-9a-f:]+(?:\])?)(?:\:[0-9]+)?)(,|,\s|$))+$"
 
 # Legitimate Accept-Language values.
-pattern_accept_language = r"^(((?:,\s*)?([a-zA-Z]{2}(?:-[a-zA-Z]{2})?|\*)(?:;(\s*)q=[0-9]\.[0-9])?)+)$"
+pattern_accept_language = r"^(((?:,\s*)?([a-zA-Z]{2}(?:-[a-zA-Z0-9]{2,3})?|\*)(?:;(\s*)q=[0-9](?:\.[0-9])?)?)+)$"
 
 # Regex matching a legitimate asset URL like http://example.org/images/example.jpg
-pattern_worthless_asset_url = r"^([a-zA-Z0-9\/\-\._]+)$"
+pattern_worthless_asset_url = r"^([a-zA-Z0-9\/\.]+)$"
