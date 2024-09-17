e99f15c16731cb29e6f181d90389210a94583d03
Pythagus <34168890+Pythagus@users.noreply.github.com>
2024-09-17 19:17:02 +0200

    :art: Update patterns compilation

--- rules/compile.py
+++ rules/compile.py
@@ -3,18 +3,19 @@ import os
 
 # Global variables.
 CURRENT_DIR = os.path.dirname(os.path.realpath(__file__))
-DELIMITER = "###"
+GLOBAL_DELIMITER = "|"
+RULE_DELIMITER = "###"
 
 
 # This function encodes a rule / set of rules using
 # the delimiter.
 def encode(type, id, input):
     if isinstance(input, str):
-        return type + DELIMITER + id + DELIMITER + input
+        return str(type + RULE_DELIMITER + id + RULE_DELIMITER + input).encode("utf-8").hex()
     
     output = ""
     for var in input:
-        output = output + "\n" + encode(type, var, input[var])
+        output = output + GLOBAL_DELIMITER + encode(type, var, input[var])
 
     return output
 
@@ -22,7 +23,7 @@ def encode(type, id, input):
 # Compile all the rules contained in the patterns.py file in
 # a way that can be treated by Splunk.
 def compile():
-    output = "version=" + str(patterns.version) + "\ndelim=" + DELIMITER
+    output = "version=" + str(patterns.version) + GLOBAL_DELIMITER + "delim=" + RULE_DELIMITER
 
     # Suspicious URL.
     output += encode("LFI", None, patterns.patterns_lfi)
@@ -31,10 +32,10 @@ def compile():
     output += encode("SQLI", None, patterns.patterns_sqli)
 
     # HTTP headers regex.
-    output += "\n" + encode("HTTP", "USER_AGENT", patterns.pattern_user_agent)
-    output += "\n" + encode("HTTP", "XFF", patterns.pattern_xff)
-    output += "\n" + encode("HTTP", "ACCEPT_LANGUAGE", patterns.pattern_accept_language)
-    output += "\n" + encode("HTTP", "WORTHLESS_ASSET_URL", patterns.pattern_worthless_asset_url)
+    output += GLOBAL_DELIMITER + encode("HTTP", "USER_AGENT", patterns.pattern_user_agent)
+    output += GLOBAL_DELIMITER + encode("HTTP", "XFF", patterns.pattern_xff)
+    output += GLOBAL_DELIMITER + encode("HTTP", "ACCEPT_LANGUAGE", patterns.pattern_accept_language)
+    output += GLOBAL_DELIMITER + encode("HTTP", "WORTHLESS_ASSET_URL", patterns.pattern_worthless_asset_url)
 
     # Encode the specific-applications patterns.
     products = patterns.patterns_custom_apps
