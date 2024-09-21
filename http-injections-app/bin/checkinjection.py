9f756d95bcb9e5233b4629daed9aeb0f8ca28d9c
Pythagus <34168890+Pythagus@users.noreply.github.com>
2024-09-21 20:15:50 +0200

    :art: Add missing command parameters

--- http-injections-app/bin/checkinjection.py
+++ http-injections-app/bin/checkinjection.py
@@ -41,29 +41,53 @@ def get_rules_from_kvstore(service: Service):
 @Configuration()
 class CheckInjectionCommand(StreamingCommand):
 
+    deep = Option(
+        doc='''
+            **Syntax:** **deep=***<str>*
+            **Description:** Determine whether all rules must be checked, or if we stop the checks when a rule matches''',
+        require=False, default=False, validate=validators.Boolean())
+
     hostname = Option(
         doc='''
             **Syntax:** **hostname=***<str>*
-            **Description:** Field containing the web server hostname like google.com''',
-        require=False)
+            **Description:** Field containing the web server hostname like example.com''',
+        require=False, validate=validators.Fieldname())
 
     url = Option(
         doc='''
             **Syntax:** **url=***<str>*
             **Description:** Field containing the accessed URL''',
-        require=False)
+        require=False, validate=validators.Fieldname())
 
     useragent = Option(
         doc='''
             **Syntax:** **useragent=***<str>*
-            **Description:** User agent used for the HTTP request ''',
-        require=False)
+            **Description:** User agent used for the HTTP request''',
+        require=False, validate=validators.Fieldname())
 
-    language = Option(
+    acceptlanguage = Option(
         doc='''
-            **Syntax:** **language=***<str>*
-            **Description:** HTTP Accept-Language parameter given by the client ''',
-        require=False)
+            **Syntax:** **acceptlanguage=***<str>*
+            **Description:** HTTP Accept-Language parameter given by the client''',
+        require=False, validate=validators.Fieldname())
+    
+    contenttype = Option(
+        doc='''
+            **Syntax:** **contenttype=***<str>*
+            **Description:** HTTP Content-Type parameter given by the client''',
+        require=False, validate=validators.Fieldname())
+    
+    cookie = Option(
+        doc='''
+            **Syntax:** **cookie=***<str>*
+            **Description:** HTTP Cookie set in the request header''',
+        require=False, validate=validators.Fieldname())
+
+    xforwardedfor = Option(
+        doc='''
+            **Syntax:** **xforwardedfor=***<str>*
+            **Description:** HTTP X-Forwarded-For set in the request header''',
+        require=False, validate=validators.Fieldname())
     
     # Determine whether the command has defined
     # the given parameter.
@@ -79,6 +103,11 @@ class CheckInjectionCommand(StreamingCommand):
             return event[self_value]
         
         return self_value
+    
+    # Determine whether the analysis shouldn't stop at
+    # first match, but check all rules.
+    def isDeepAnalysis(self):
+        return getattr(self, 'deep') or False
 
     # This method is called by splunkd before the
     # command executes. It is used to get the configuration
@@ -96,30 +125,67 @@ class CheckInjectionCommand(StreamingCommand):
 
         # Build the regexes and local stuff.
         try:
-            patterns.build(rules=http_rules)
+            patterns.build(rules=http_rules, deep=self.isDeepAnalysis())
         except patterns.HttpInjectionRegexCompilationFailure as e:
             self.write_error(repr(e))
             self.write_error("HttpInjections: Failed to compile regex: %s" % e.regex_key)
             exit(ERR_REGEX_COMPILATION)
 
+    # Determine whether the event matches one of the suspicious
+    # rules in place.
+    def check_rules(self, event):
+        deep = self.isDeepAnalysis()
+        rules_triggered = []
+        should_continue = True
+
+        # Check the User-Agent.
+        if self.hasParameter('useragent') and http.is_suspicious_user_agent(self.getParamValue('useragent', event)):
+            rules_triggered.append("USER_AGENT")
+            should_continue = deep
+
+        # Check the Accept-Language.
+        if should_continue and self.hasParameter('acceptlanguage') and http.is_suspicious_accept_language(self.getParamValue('acceptlanguage', event)):
+            rules_triggered.append("ACCEPT_LANGUAGE")
+            should_continue = deep
+
+        # Check the Content-Type.
+        if should_continue and self.hasParameter('contenttype') and http.is_suspicious_content_type(self.getParamValue('contenttype', event)):
+            rules_triggered.append("CONTENT_TYPE")
+            should_continue = deep
+
+        # Check the Cookie.
+        if should_continue and self.hasParameter('cookie') and http.is_suspicious_cookie(self.getParamValue('cookie', event)):
+            rules_triggered.append("COOKIE")
+            should_continue = deep
+
+        # Check the X-Forwarded-For.
+        if should_continue and self.hasParameter('xforwardedfor') and http.is_suspicious_xff(self.getParamValue('xforwardedfor', event)):
+            rules_triggered.append("X_FORWARDED_FOR")
+            should_continue = deep
+
+        # Check the URL.
+        if should_continue and self.hasParameter('url'):
+            suspicious_url_rule = http.is_suspicious_url(self.getParamValue('url', event))
+
+            if suspicious_url_rule:
+                if isinstance(suspicious_url_rule, list):
+                    rules_triggered = rules_triggered + suspicious_url_rule 
+                else:
+                    rules_triggered.append(suspicious_url_rule)
+                    
+                should_continue = deep
+        
+        return rules_triggered
+
     # This is the method treating all the events.
     def stream(self, events):
+        if self.isDeepAnalysis():
+            msg = "HttpInjections: Deep analysis takes more time!"
+            self.write_warning(msg)
+            self.logger.warning(msg)
+
         for event in events:
-            rules_triggered = []
-
-            if self.hasParameter('useragent') and http.is_suspicious_user_agent(self.getParamValue('useragent', event)):
-                rules_triggered.append("USER_AGENT")
-
-            if self.hasParameter('language') and http.is_suspicious_accept_language(self.getParamValue('language', event)):
-                rules_triggered.append("ACCEPT_LANGUAGE")
-
-            if self.hasParameter('url'):
-                suspicious_url_rule = http.is_suspicious_url(self.getParamValue('url', event))
-
-                if suspicious_url_rule:
-                    rules_triggered.append(suspicious_url_rule)
-
-            event["rules_triggered"] = rules_triggered
+            event["rules_triggered"] = self.check_rules(event)
 
             yield event
 
